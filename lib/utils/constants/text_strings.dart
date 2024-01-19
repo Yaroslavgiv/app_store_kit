cd5b1f3263a57dd43810e94d6d5105407750eccb
Yaroslavgiv <yaroslavgiv31@gmail.com>
2024-01-19 23:34:53 +0300

    first commit

---
+++ lib/utils/constants/text_strings.dart
@@ -0,0 +1,32 @@
+/// This class contains all the App Text in String formats.
+/// 
+class  TTexts {
+  // -- GLOBAL Texts
+  static const String and = "и";
+  static const String skip = "Пропустить";
+  static const String done = "Готово";
+  static const String submit = "Отправить";
+  static const String appName = "T-Store";
+  static const String tContinue = "Продолжить";
+
+  // -- Authentication Forms
+  static const String firstName = "Имя";
+  static const String lastName = "Фамилия";
+  static const String email = "E-Mail";
+  static const String password = "Пароль";
+  static const String newPassword = "Новый пароль";
+  static const String username = "Имя пользователя";
+  static const String phoneNo = "Телефон";
+  static const String rememberMe = "Запомнить меня";
+  static const String forgetPassword = "Забыл пароль?";
+  static const String signIn = "Вход";
+  static const String createAccount = "Создать аккаунт";
+  static const String orSignInWith = "или войдите в систему с помощью";
+  static const String orSignUpWith = "или зарегестрируйтесь с помощью";
+  static const String iAgreeTo = "Я согласен";
+  static const String privacyPolicy = "Политика конфиденциальности";
+  static const String termsOfUse = "Условия использования";
+  static const String verificationCode = "код подтверждения";
+  static const String resendEmail = "Повторно отправить Email";
+  static const String resendEmailIn = "Повторно отправить email в";
+}
